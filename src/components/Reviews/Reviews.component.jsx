279e28ae38777736e7eae8dee6767f0cd3f91978
Keith Gibson <keithgibson@Keiths-MacBook-Air.local>
2023-03-30 19:44:18 +0100

    Fixing errors with firebase promise

--- src/components/Reviews/Reviews.component.jsx
+++ src/components/Reviews/Reviews.component.jsx
@@ -1,20 +1,27 @@
-import './Reviews.style.css'
-import { getReviews, addReview } from '../../firebase'
+import './Reviews.style.css';
+import { getReviews, addReview } from '../../firebase';
+import { useEffect, useState } from 'react';
 
 const Reviews = () => {
-    const allReviews = getReviews()
-    console.log(allReviews)
-    return (
-        <div>
-          allReviews.map((review) => {
-            return (
-                <div className="review">
-                    <h3>{review.name}</h3>
-                    <p>{review.review}</p>
-       </div>
-        
-        
-    )
-}
 
-export default Reviews
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        const allReviews = getReviews();
+        console.log(allReviews)
+        setReviews(allReviews);
+    }, [])
+
+    const reviewElements = reviews.map((review) => {
+        return <div className='review'>
+				<h3>{review.name}</h3>
+				<p>{review.review}</p>
+		</div>
+    })
+	
+    // console.log(reviews)
+	return reviewElements
+	
+};
+
+export default Reviews;
